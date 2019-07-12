b48021203f0d1c863338e8b5233b2405e822cbcc
Marcin Jabłoński <marcin.k.jablonski@gmail.com>
2019-07-12 19:26:27 +0200

    V1 of DH character creator

---
+++ precache-manifest.e001b5e8c97ba9591ddd770033672ed3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8ad22bd9b4581dd722ab9b9129f8b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b4b8523f0ad6b4b2922",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "981fcd58dde2f2d5dfd6",
+    "url": "/static/js/2.ecdda2ad.chunk.js"
+  },
+  {
+    "revision": "9b4b8523f0ad6b4b2922",
+    "url": "/static/js/main.92ac7958.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
